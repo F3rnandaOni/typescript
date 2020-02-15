fb54c46991e1a2df84bfd5a394fffbf5f16fe067
Dudu <ec943077@gmail.com>
2020-02-15 01:44:00 +0000

    Dia 14.02

---
+++ types/gerente.ts
@@ -0,0 +1,14 @@
+namespace empresa{
+    class Gerente extends Funcionario{
+        private setor: string;
+
+        getSetor(): string{
+            return this.getSetor();
+        }
+
+        setSetor(setor: string): void{
+            this.setor = setor;
+        }
+
+    }
+}
