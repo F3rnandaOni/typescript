8f6c98f20c03b72181e150b98c9a97e8fd83380f
F3rnandaOni <fernandasilvaoni@gmail.com>
2020-03-12 01:42:24 +0000

    Aula de TypeScrypt

--- hospital/instance.ts
+++ hospital/instance.ts
@@ -1,7 +1,7 @@
 namespace hospital{
-    let m = new Medico("Domitilio","120.120.120.00");
-    m.setCrm(59346700);
-    m.setEspecialidade("Neurocirurgião");
+    let m = new Medico("Josué","158.976.423.22");
+    m.setCrm(87450978);
+    m.setEspecialidade("Psicologo");
 
 
     document.getElementById("nomeM").textContent =  m.getNome();
@@ -9,9 +9,9 @@ namespace hospital{
     document.getElementById("crm").textContent = m.getCrm().toString();
     document.getElementById("espM").textContent = m.getEspecialidade();
 
-    let m2 = new Medico("Henrique","130.130.130.00");
-    m2.setCrm(79823466);
-    m2.setEspecialidade("Psiquiatra");
+    let m2 = new Medico("hémersom","521.890.476.33");
+    m2.setCrm(98457809);
+    m2.setEspecialidade("Oftalmologista");
 
 
     document.getElementById("nomeM2").textContent =  m2.getNome();
@@ -19,9 +19,9 @@ namespace hospital{
     document.getElementById("crm2").textContent = m2.getCrm().toString();
     document.getElementById("espM2").textContent = m2.getEspecialidade();
 
-    let m3 = new Medico("Januário","140.140.140.00");
-    m3.setCrm(55566678);
-    m3.setEspecialidade("Cardiologista");
+    let m3 = new Medico("Eleonor","103.478.923.99");
+    m3.setCrm(45671209);
+    m3.setEspecialidade("Dermatologista");
 
 
     document.getElementById("nomeM3").textContent =  m3.getNome();
@@ -30,51 +30,51 @@ namespace hospital{
     document.getElementById("espM3").textContent = m3.getEspecialidade();
 
 
-    let e = new Enfermeiro("Adelaidy","112.112.112-12");
-    e.setCoren(47682239);
+    let e = new Enfermeiro("Nancy","628.890.143-54");
+    e.setCoren(89566723);
 
     document.getElementById("nomeE").textContent =  e.getNome();
     document.getElementById("cpfE").textContent = e.getCpf();
     document.getElementById("coren").textContent = e.getCoren().toString();
 
-    let e2 = new Enfermeiro("Dionísio","114.114.114-14");
-    e2.setCoren(33349268);
+    let e2 = new Enfermeiro("Igor","490.823.178-67");
+    e2.setCoren(64291234);
 
     document.getElementById("nomeE2").textContent =  e2.getNome();
     document.getElementById("cpfE2").textContent = e2.getCpf();
     document.getElementById("coren2").textContent = e2.getCoren().toString();
 
-    let e3 = new Enfermeiro("Anselmo","116.116.116-16");
-    e3.setCoren(11332297);
+    let e3 = new Enfermeiro("Diogo","519.823.116-43");
+    e3.setCoren(12344321);
 
     document.getElementById("nomeE3").textContent =  e3.getNome();
     document.getElementById("cpfE3").textContent = e3.getCpf();
     document.getElementById("coren3").textContent = e3.getCoren().toString();
 
 
-    let p = new Paciente("Açussena", "555.555.555-55");
-    p.setCodPaciente(749);
+    let p = new Paciente("Varyo", "140.389.543-09");
+    p.setCodPaciente(825);
 
     document.getElementById("nomeP").textContent =  p.getNome();
     document.getElementById("cpfP").textContent = p.getCpf();
     document.getElementById("codP").textContent = p.getCodPaciente().toString();
 
-    let p2 = new Paciente("Willian", "553.553.553-53");
-    p2.setCodPaciente(222);
+    let p2 = new Paciente("Ligiane", "789.000.016-34");
+    p2.setCodPaciente(092);
 
     document.getElementById("nomeP2").textContent =  p2.getNome();
     document.getElementById("cpfP2").textContent = p2.getCpf();
     document.getElementById("codP2").textContent = p2.getCodPaciente().toString();
 
-    let p3 = new Paciente("Sergyo", "550.550.550-50");
-    p3.setCodPaciente(713);
+    let p3 = new Paciente("Elias", "000.000.000-40");
+    p3.setCodPaciente(313);
 
     document.getElementById("nomeP3").textContent =  p3.getNome();
     document.getElementById("cpfP3").textContent = p3.getCpf();
     document.getElementById("codP3").textContent = p3.getCodPaciente().toString();
 
     let h = new Hospital();
-    h.setNome("Emílio Benícios");
+    h.setNome("Camila Mendes");
     h.addEnfermeiro(e);
     h.addEnfermeiro(e2);
     h.addEnfermeiro(e3);
